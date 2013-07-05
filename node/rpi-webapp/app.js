4a31a712e328fcaca2e9db78cd6fce64a135d065
jaguarnac <=>
2013-07-05 08:00:43 +0000

    Changed the web frontend to handle 3 LEDs and blink them in two different patterns.

--- node/rpi-webapp/app.js
+++ node/rpi-webapp/app.js
@@ -1,31 +1,89 @@
 var express = require('express');
 var OutputChannel = require('../Channel').OutputChannel;
 
-var app = express(),
-    c11 = new OutputChannel(11);
+var app = express();
+var channels = [ new OutputChannel(11),
+                 new OutputChannel(15),
+                 new OutputChannel(16)  ];
+var activePattern = null;
 
-app.get('/hello.txt', function(req, res){
-  var body = 'Hello, World';
-  res.send('Hello, World');
-});
-
-app.get('/on', function(req, res){
-  c11.turnOn();
-  res.send('success');
-});
-
-app.get('/off', function(req, res){
-  c11.turnOff();
-  res.send('success');
-});
-
-app.get('/led', function(req, res){
-  var html="<!DOCTYPE html><html><body><a href='?status=on'>on</a> <a href='?status=off'>off</a></body></html>";
-  if ( req.param('status') === 'on' ){
-    c11.turnOn();
-  } else {
-    c11.turnOff();
+var pattern1 = new function(){
+  var i = 0;
+  var tim = null;
+  function blink(){
+    channels[i].toggle();
+    i++;
+    if (i == channels.length) { i=0; }
+    tim = setTimeout(blink, 500)
   }
+  this.start = function(){
+    i=0;
+    blink();
+  }
+  this.stop = function(){
+    clearTimeout(tim);
+  }
+}
+
+var pattern2 = new function(){
+  var i = 0;
+  var tim = null;
+  function blink(){
+    channels[i].toggle();
+    i++;
+    if (i == channels.length) { i=0; }
+    channels[i].toggle();
+    tim = setTimeout(blink, 500)
+  }
+  this.start = function(){
+    i=0;
+    tim = setTimeout(blink, 500);
+    channels[0].toggle();
+  }
+  this.stop = function(){
+    clearTimeout(tim);
+  }
+}
+
+app.get('/', function(req, res){
+  var html, action, channel;
+  action = req.param('action');
+  
+  if (action === 'on') {
+    channel = req.param('channel');
+    channels[channel].turnOn();
+  } else if (action === 'off'){
+    channel = req.param('channel');
+    channels[channel].turnOff();
+  } else if (action === 'alloff'){
+    if (activePattern && activePattern.stop){ 
+      activePattern.stop(); 
+    }
+    for (var i=0; i<channels.length; i++){
+      channels[i].turnOff();
+    }
+  } else if (action === 'pattern1'){
+    pattern1.start();
+    activePattern = pattern1;
+  } else if (action === 'pattern2'){
+    pattern2.start();
+    activePattern = pattern2;
+  }
+  
+ 
+  html="<!DOCTYPE html><html><body>";
+  for (var i=0; i<channels.length; i++){
+    html += '<div>';
+    html += ' CHANNEL '+i+' : ';
+    html += '<a href="/?action=on&channel='+i+'">ON</a>  ';
+    html += '<a href="/?action=off&channel='+i+'">OFF</a>  ';
+    html += '</div>';
+  }
+  html += "<div><a href='/?action=pattern1'>Pattern 1</a> </div>"; 
+  html += "<div><a href='/?action=pattern2'>Pattern 2</a> </div>";
+  html += "<div><a href='/?action=alloff'>ALL OFF</a> </div>";
+  html += "</body></html>";
+  
   res.send(html);
   
 });
