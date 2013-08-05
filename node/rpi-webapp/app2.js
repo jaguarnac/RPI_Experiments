2aed53e0f8b4bc26d57c913daa0f19c2a21bd0eb
jaguarnac <=>
2013-08-05 06:42:30 +0000

    Added LED control to server.

--- node/rpi-webapp/app2.js
+++ node/rpi-webapp/app2.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var OutputChannel = require('../Channel').OutputChannel;
+var LEDStrip = require('RPi-LPD8806').LEDStrip;
 
 var app = express();
 var channels = [ new OutputChannel(16) ];
+var ledstrip = new LEDStrip(32);
 
 app.get('/', function(req, res){
-  var html, action, channel;
+  var html, action, channel, r, g, b;
   action = req.param('action');
   
   if (action === 'blink') {
@@ -14,7 +16,19 @@ app.get('/', function(req, res){
     setTimeout ( function(){
       channels[channel].turnOff();
     },500);
-  }   
+  }
+
+  if (action ==='rgbfill'){
+    r = req.param('r');
+    g = req.param('g');
+    b = req.param('b');
+    ledstrip.fillRGB(r,g,b);
+    ledstrip.update();
+  }
+
+  if (action === 'ledoff'){
+    ledstrip.all_off();
+  }
   
  
   html="<!DOCTYPE html><html><body>";
@@ -24,6 +38,16 @@ app.get('/', function(req, res){
     html += '<a href="/?action=blink&channel='+i+'">ON</a>  ';
     html += '</div>';
   }
+  html += '<br /> <br />';
+  html += '<a href="/?action=rgbfill&r=255&g=0&b=0">RED</a> <br />';
+  html += '<a href="/?action=rgbfill&r=0&g=255&b=0">GREEN</a> <br />';
+  html += '<a href="/?action=rgbfill&r=0&g=0&b=255">BLUE</a> <br />';
+  html += '<a href="/?action=rgbfill&r=255&g=255&b=255">WHITE</a> <br />';
+  html += '<br /> <br />';
+
+  html += '<a href="/?action=ledoff">OFF</a> <br />';
+  html += '<br /> <br />';
+
   html += "</body></html>";
   
   res.send(html);
